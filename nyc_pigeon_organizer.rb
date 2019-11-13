5e5a49cdd5efb26745f8dce7f7500deb3875c7aa
Alexander Williams <williams_alex@protonmail.com>
2019-11-13 22:35:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,26 +1,18 @@
-#def nyc_pigeon_organizer(data)
-#  names = []
-#  names << data[:gender][:male]
-#  names << data[:gender][:female]
-#  pigeon_list = Hash[names.collect {|item| [item, {:color => [], :gender => [], :lives => []}]}]
-#  for property in data do
-#    for value in property do
-#      for owner in value do
-#        pigeon_list[owner][property] = value
-#      end
-#    end
-#  end
-#  return pigeon_list
-#end
-
-
-#def nyc_pigeon_organizer(pigeon_data)
-#  names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
-#  pigeon_data.each do |key, hash|
-#    for pigeon in names do
-#      hash.select{|key, hash| hash == pigeon}
-      
 def nyc_pigeon_organizer(pigeon_data)
-  names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
-  
+    names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
+    datatypes = []
+    pigeon_list = {}
+    pigeon_data.each {|key, value| datatypes << key}
+    for pigeon in names do
+        pigeon_list.store(pigeon, {})
+        for type in datatypes do
+            x = []
+            x = pigeon_data[type].select{|key, hash| hash.include? pigeon}
+            x = x.keys
+            x.map! {|item| item.to_s[0..]}
+            pigeon_list[pigeon].store(type, x)
+        end
+    end
+    return pigeon_list
 end
+
