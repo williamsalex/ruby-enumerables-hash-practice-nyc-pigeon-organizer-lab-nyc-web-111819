0f5dae68ba1233543e99a7fec3da239f2c0f47ab
Alexander Williams <williams_alex@protonmail.com>
2019-11-13 22:35:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+    names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
+    datatypes = []
+    pigeon_list = {}
+    pigeon_data.each {|key, value| datatypes << key}
+    for pigeon in names do
+        pigeon_list.store(pigeon, {})
+        for type in datatypes do
+            x = []
+            x = pigeon_data[type].select{|key, hash| hash.include? pigeon}
+            x = x.keys
+            x.map! {|item| item.to_s[0..]}
+            pigeon_list[pigeon].store(type, x)
+        end
+    end
+    return pigeon_list
 end
+
