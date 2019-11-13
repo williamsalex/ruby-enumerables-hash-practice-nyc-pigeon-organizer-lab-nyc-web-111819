5cf2e576d15b6459ce231f0e434371652a534dcc
Alexander Williams <williams_alex@protonmail.com>
2019-11-13 21:26:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+#def nyc_pigeon_organizer(data)
+#  names = []
+#  names << data[:gender][:male]
+#  names << data[:gender][:female]
+#  pigeon_list = Hash[names.collect {|item| [item, {:color => [], :gender => [], :lives => []}]}]
+#  for property in data do
+#    for value in property do
+#      for owner in value do
+#        pigeon_list[owner][property] = value
+#      end
+#    end
+#  end
+#  return pigeon_list
+#end
+
+
+#def nyc_pigeon_organizer(pigeon_data)
+#  names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
+#  pigeon_data.each do |key, hash|
+#    for pigeon in names do
+#      hash.select{|key, hash| hash == pigeon}
+      
+def nyc_pigeon_organizer(pigeon_data)
+  names = pigeon_data[:gender][:male] + pigeon_data[:gender][:female]
+  
 end
